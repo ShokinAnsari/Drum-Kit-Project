fe33eba12e85bbd31fcf11da5652b1e568423261
Shokin Ansari <123631138+ShokinAnsari@users.noreply.github.com>
2025-02-19 18:00:42 +0530

    index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ let numberofbtns = document.querySelectorAll(".drum").length
 // detect which button clicked
 for (let i = 0; i < numberofbtns; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        // var audio=new Audio("sounds/tom-1.mp3")
-        // audio.play()
+
         var buttonHTML = this.innerHTML;
     
         makesound(buttonHTML)
@@ -28,32 +27,32 @@ function makesound(key)
 {
     switch (key) {
         case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3")
+            var tom1 = new Audio("tom-1.mp3")
             tom1.play()
             break;
 
         case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3")
+            var tom2 = new Audio("tom-2.mp3")
             tom2.play()
             break;
         case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3")
+            var tom3 = new Audio("tom-3.mp3")
             tom3.play()
             break;
         case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3")
+            var tom4 = new Audio("tom-4.mp3")
             tom4.play()
             break;
         case "j":
-            var crash = new Audio("sounds/crash.mp3")
+            var crash = new Audio("crash.mp3")
             crash.play()
             break;
         case "k":
-            var kick_bass = new Audio("sounds/kick-bass.mp3")
+            var kick_bass = new Audio("kick-bass.mp3")
             kick_bass.play()
             break;
         case "l":
-            var snare = new Audio("sounds/snare.mp3")
+            var snare = new Audio("snare.mp3")
             snare.play()
             break;
 
